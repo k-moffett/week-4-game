e0bcb4641167b73af23d9aca5964882a3367af81
Kurtis Moffett <moffkr91@gmail.com>
2018-02-19 13:31:46 -0800

    values assigned. need to make it better

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,30 +8,32 @@
     //When the player "#total-score" is equal to "#random-number", "#display-message" should show a "You win!" message, then increase "#win-counter" by one and reset "#random-number", set "#total-score" to 0, and reset jewel values
     // When the player "#total-score" > than "#random-number","#display-message" should show a "You lose!" message  increase "#loss-counter" by one and reset "#random-number", set "#total-score" to 0, and reset jewel values.
 
-$( document ).ready(function() {
+$( document ).ready(function () {
     
 var game = {
     random_number: "",
     total_score: "",
-    jewel_1: "",
-    jewel_2: "",
-    jewel_3: "",
-    jewel_4: "",
+    gem_1: "",
+    gem_2: "",
+    gem_3: "",
+    gem_4: "",
     flag: 0,
     flag_2: 0,
     start: 
         function start() {
             var random_number_array = [20,21,22,23,24,25,26,27,28,29,30,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40]
             this.random_number = random_number_array[Math.floor(Math.random() * random_number_array.length)];
-            var random_jewel_array = [1,2,3,4,5,6,7,8,9]
-            $("#jewel-1").attr("value", random_jewel_array[Math.floor(Math.random() * random_jewel_array.length)])
-            $("#jewel-2").attr("value", random_jewel_array[Math.floor(Math.random() * random_jewel_array.length)])
-            $("#jewel-3").attr("value", random_jewel_array[Math.floor(Math.random() * random_jewel_array.length)])
-            $("#jewel-4").attr("value", random_jewel_array[Math.floor(Math.random() * random_jewel_array.length)])
+            var random_gem_array = [1,2,3,4,5,6,7,8,9]
+            $("#total-score").html(0);
+            $("#gem-1").attr("value", random_gem_array[Math.floor(Math.random() * random_gem_array.length)])
+            $("#gem-2").attr("value", random_gem_array[Math.floor(Math.random() * random_gem_array.length)])
+            $("#gem-3").attr("value", random_gem_array[Math.floor(Math.random() * random_gem_array.length)])
+            $("#gem-4").attr("value", random_gem_array[Math.floor(Math.random() * random_gem_array.length)])
             $("#random-number").html(this.random_number);
             $("#win-counter").html(0);
             $("#loss-counter").html(0);
             $("#total-score").html(0);
+
             
         },
     restart:
@@ -41,11 +43,24 @@ var game = {
             var random_jewel_array = [1,2,3,4,5,6,7,8,9]
             $("#random-number").html(this.random_number);
             $("#total-score").html(0);
-            $("#jewel-1").attr("value", random_jewel_array[Math.floor(Math.random() * random_jewel_array.length)])
-            $("#jewel-2").attr("value", random_jewel_array[Math.floor(Math.random() * random_jewel_array.length)])
-            $("#jewel-3").attr("value", random_jewel_array[Math.floor(Math.random() * random_jewel_array.length)])
-            $("#jewel-4").attr("value", random_jewel_array[Math.floor(Math.random() * random_jewel_array.length)])
+            $("#gem-1").attr("value", random_gem_array[Math.floor(Math.random() * random_gem_array.length)])
+            $("#gem-2").attr("value", random_gem_array[Math.floor(Math.random() * random_gem_array.length)])
+            $("#gem-3").attr("value", random_gem_array[Math.floor(Math.random() * random_gem_array.length)])
+            $("#gem-4").attr("value", random_gem_array[Math.floor(Math.random() * random_gem_array.length)])
         },  
+}
+
+function add_jewel_values() {
+
+}
+function compare_values() {
+
+}
+function win() {
+
+}
+function lose() {
+
 }
 
 
@@ -54,10 +69,8 @@ var game = {
 
 
 
-
-
-
-
-
+$(".gems").on("click", function get_jewel_values() {
+    console.log("working")
+})
 game.start()
       });
